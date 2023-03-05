e2e93c014154c49b475cc6f8341c26db8d9a1ab2
inesgpinto <inesgpinto@tecnico.ulisboa.pt>
2023-03-05 19:28:10 +0000

    chosing best molecules based on f, function for mating

--- main.cpp
+++ main.cpp
@@ -1,52 +1,65 @@
 #include <cstdio>
 #include <iostream>
 #include <iomanip>
-#include <vector>
 #include <algorithm>
+#include <vector>
 #include "molecula.h"
 
 using namespace std;
 
 int main()
 {
-  int N_moleculas = 3;
+  int N_moleculas = 10;
   int N_atomos = 10;
   int dim_caixa = 10;
+  double survival_rate = 0.4;
   
-  double f_value;
-  
-  /*molecula* mol = new molecula(N_atomos, dim_caixa);
-  
-  f_value = mol -> Potencial();
-
-  cout << f_value << endl;
-  */
-
+  //Inicializar população como vector de ponteiros de objetos
   vector<molecula*> pop;
-  
+
+  //Preencher o vector
   for(int i=0; i<N_moleculas; ++i)
     pop.push_back(new molecula(N_atomos, dim_caixa));
 
   
   for(int i=0; i<N_moleculas; ++i)
-    pop[i]->Potencial();
+    cout << pop[i]->Potencial() << endl;
 
-  for(int i=0; i<N_moleculas; ++i)
-    cout << pop[i]->Get_Fvalue() << endl;
-  
-  sort(pop.begin(), pop.end(), greater<molecula*>());
+  //Faz a ordenação
+  sort(pop.begin(), pop.end(), molecula::LessPot);
 
   cout << "====== Sorting Done ======" << endl;
 
   for(int i=0; i<N_moleculas; ++i)
-    cout << pop[i]->Get_Fvalue() << endl;
-  
-  
-  /*
-  gRandom = new TRandom3(0);
-  double x = gRandom->Uniform(-1.,1.);
-  cout << x << endl;
-  */
-  
+    cout << pop[i]->Potencial() << endl;
+
+  //cout << "====== Check if Sorting is well made ======" << endl;
+
+  //for(int i=0; i<N_moleculas-1; ++i)
+  //  cout << pop[i]->Potencial() - pop[i+1]->Potencial() << endl;
+
+  //4 parents -> 2 couples
+  // 3 children for each
+  int parents_nb = int(survival_rate * N_moleculas + 0.5);
+  int couples_nb = parents_nb/2;
+  int children_nb = N_moleculas - parents_nb;
+  int children_per_couple = children_nb / couples_nb;
+  for(int i = 0; i<couples_nb;i++){
+    for (int j = 0; j < children_per_couple; j++){
+      double gene_prop = 1./(j+2);
+      pop[parents_nb+i*children_per_couple+j]->Mating(pop[couples_nb*i],pop[couples_nb*i+1], gene_prop);
+      //cout << parents_nb+i*children_per_couple+j << endl;
+      //cout<<couples_nb*i<<" "<<couples_nb*i+1<<endl;
+
+
+
+    }
+  }  
+
+    cout<<"NEXT GEN"<<endl;
+    for(int i=0; i<N_moleculas; ++i)
+      cout << pop[i]->Potencial() << endl;
+
+
   return 0;
 }
--- molecula.h
+++ molecula.h
@@ -16,25 +16,31 @@ public:
 
   //Constructors and Destructor
   molecula(int n_atomos=0, int dim_caixa=0);
+  molecula(molecula* mom, molecula* dad, double gene_prop, int n_atomos=0);
   ~molecula();
 
   //Getters
   int Get_Natomos() {return N_atomos;}
   int Get_Dim() {return Dim_caixa;}
+  double** Get_Pos() {return posicoes;}
+  double Get_Pot(){return f_value;}
   
   //Comparator
-  static bool LessPot(molecula* mol1, molecula* mol2) {return mol1->Potencial() < mol2->Potencial();}
+  static bool LessPot(molecula* mol1, molecula* mol2) {return mol1->Get_Pot() < mol2->Get_Pot();}
 
   //Calculations
   double Potencial();
   double OtherPotential();
+
+  void Mating(molecula* mom, molecula* dad, double gene_prop=0);
+  
 
 private:
   int N_atomos;
   int Dim_caixa;
 
   double **posicoes;
-  //double f_value;
+  double f_value;
   //vector< pair < int, float> > f_value; 
 };
 
