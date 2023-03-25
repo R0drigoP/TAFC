5298ea1226835650da44426b2c0684e60ddedf0a
R0drigoP <rmcpereira.6@gmail.com>
2023-03-25 23:40:08 +0000

    everything in english and adaptative mutation

---
+++ final/main.cpp
@@ -0,0 +1,204 @@
+#include "molecule.h"
+#include "TGraph.h"
+#include "TCanvas.h"
+#include "TAxis.h"
+#include "TH1F.h"
+#include <ctime>
+#include <fstream>
+#include "global.h"
+
+using namespace std;
+
+//global variables
+unsigned int N_molecules = 100, N_atoms = 13;
+float L_box = 2., survival_rate = 0.1, mutation_prob = 0.05, sex_prob = 0.3;
+float alpha = 5e-2, m0 = 0.2;
+unsigned int max_iter = 10000;
+
+
+unsigned int parents_nb = int(survival_rate * N_molecules);
+//int couples_nb = int(parents_nb/2);
+//int children_per_couple = ( N_moleculas - parents_nb) / couples_nb;
+
+bool mating = 0;
+
+unsigned int nb_of_calls = 0, nb_of_calls_mute = 0, nb_of_calls_mat = 0, nb_of_calls_mat_plano = 0;
+
+double final_fit = 0.;
+
+
+//probability of each molecule to be a parent
+int main(){
+
+
+  if(mating==1 &&  N_molecules * survival_rate < 2.){
+    cout<<"To have sexual reprodution at least 2 molecules must survive each gen..."<<endl;
+    cout<<"Increase your population or the survival probability"<<endl;
+    return 1;
+  }
+
+  if(survival_rate < 0. || survival_rate > 1. || parents_nb >= N_molecules){
+    cout << "Check your survival_rate" << endl;
+    return 1;
+  }
+
+
+  double **positions;
+  positions = new double*[N_atoms];
+  for (int i = 0; i < N_atoms; i++) 
+    positions[i] = new double[3];
+
+  int* flag = new int[N_molecules];
+  for (int i = 0; i < N_molecules; i++) 
+    flag[i] = 0;
+
+
+  TCanvas *c1 = new TCanvas();
+  auto gr = new TGraph();
+
+  ofstream text_file("best_molecule.bs");
+  ofstream movie_file("best_molecule.mv");
+  
+  //population of molecules
+  vector<molecule*> pop;
+
+
+  for(int i = 0; i < N_molecules; ++i)
+    pop.push_back(new molecule(N_atoms, L_box, mutation_prob));
+ 
+  // is_parent[i] == 0 : False: Molecule i is not a parent
+  // is_parent[i] == 1 : True: Molecule i is not a parent
+  //bool *is_parent = new bool[N_moleculas];
+  //int *parent_order = new int[couples_nb*2];
+
+  for(int iter = 0; iter < max_iter; iter++){
+
+    //cout << "ITER NR " << iter << endl;
+    /*
+    for(int i = 0; i < N_moleculas; i++){
+      cout<<"Molecula"<<i<<endl;
+      positions = pop[i] -> Get_Pos();
+        cout << "Atomo " << 0 << " : " <<positions[0][0] << ", " << positions[0][1] << ", " << positions[0][2] << endl;
+        cout << "Atomo " << 1 << " : " <<positions[1][0] << ", " << positions[1][1] << ", " << positions[1][2] << endl;
+        cout << "Atomo " << 2 << " : " <<positions[2][0] << ", " << positions[2][1] << ", " << positions[2][2] << endl;
+
+      }*/
+
+    //cout<<"got potential"<<endl;
+
+    sort(pop.begin(), pop.end(), molecule::LessPot);
+
+    //cout<<"sorted"<<endl;
+
+    if(iter == 1){
+      double** best_pos = pop[0]->Get_Pos();
+      for(int i = 0; i < N_atoms; ++i){
+        text_file << "atom C " << flush;
+        for(int j = 0; j < 3; ++j)
+          text_file << best_pos[i][j] << " " << flush;
+        text_file << endl;
+      }
+    }
+    else if(iter%1000 == 0){
+      movie_file << "frame" << endl;
+      double** best_pos = pop[0]->Get_Pos();
+      for(int i = 0; i < N_atoms; ++i){
+        for(int j = 0; j < 3; ++j)
+          movie_file << best_pos[i][j]/L_box << " ";
+      }
+      movie_file << endl << endl;
+
+      //cout << "ITER NR " << iter << endl;
+
+      //cout << "Pot so far " << pop[0]->Get_Fit() << endl;
+
+      //cout<<"nb of mutations "<<nb_of_calls_mute<<endl;
+      //cout<<"nb of mat plano "<<nb_of_calls_mat_plano<<endl;
+      //cout<<"nb of mat       "<<nb_of_calls_mat<<endl;
+      //cout<<"pop x iter      "<<N_molecules*(iter+1)<<endl;
+      //cout<<"total reproduti "<<nb_of_calls_mute+nb_of_calls_mat_plano+nb_of_calls_mat<<endl;
+      //cout<<"nb of func calss"<<nb_of_calls<<endl;
+
+    }
+    
+    gr -> AddPoint( iter, pop[0] -> Get_Fit());
+ 
+    //matar os mais fracos -> fazer copias da melhor pop (se calhar atribuir alguma aleatoriadade a este processo)
+    for(int mol = survival_rate*N_molecules; mol < N_molecules; mol += survival_rate*N_molecules){              
+      int alive = 0;
+      while(alive < survival_rate*N_molecules && (mol+alive)<N_molecules){
+        //cout<<mol<<" "<<alive<<endl;                          
+        pop[mol+alive] -> Set_Pos(pop[alive] -> Get_Pos());
+        pop[mol+alive] -> Set_Fit(pop[alive] -> Get_Fit());
+
+        ++alive;
+      }
+    }
+
+    if( mating == 1){
+      //setting flag to 0 for parents
+      for(int i = 0 ; i < parents_nb; i++)
+        flag[i] = 0;
+
+      //sexual reproduction
+      for(int i = parents_nb ; i <N_molecules; i++)
+        flag[i] = pop[i]->generate_children3( pop);
+    }
+
+    //assexual reproduction
+    for(int mol = 0; mol < N_molecules; mol++){
+      pop[mol] -> Mutate(iter, m0, alpha, flag[mol]);
+    }
+
+    
+    if( iter == max_iter-1){
+      positions = pop[0] -> Get_Pos();
+      final_fit =  pop[0]-> Get_Fit();
+      cout << "Final Pot " << final_fit << endl;
+
+
+      
+      //for(int i = 0; i < N_atomos; i++)
+      //cout << "Atomo " << i << " : " << positions[i][0] << ", " << positions[i][1] << ", " << positions[i][2] << endl;
+    }
+    
+  }
+  
+  //delete[] is_parent;
+  //delete[] parent_order;
+  
+  for(int i = 0; i < 3; ++i) 
+    delete[] positions[i];
+  
+  delete[] positions;
+  delete[] flag;
+  //Forma correta de destruir o vetor mas dá seg fault
+  //for(vector<molecula*>::iterator i = pop.begin(); i != pop.end(); ++i)
+  //delete *i;
+
+
+  
+  pop.clear();
+  double atom_size = 0.1/L_box;
+  
+  text_file << endl << "spec C 0.1 Red" << endl
+	    << endl << "bonds C C 0.5 1.5 0.01 0.0"
+	    << endl << "bonds C H 0.4 1.0 0.01 1.0"
+	    << endl << "scale 100"
+	    << endl << "inc 5.0" << flush;
+  
+  c1 -> cd();
+  gr->GetHistogram()->SetMaximum(-0.1*final_fit);
+  gr->GetHistogram()->SetMinimum(1.01*final_fit);
+
+  gr -> Draw("AP");
+  c1 -> SaveAs("evolution.pdf");
+  
+  text_file.close();
+  movie_file.close();
+
+
+  cout<<"Total pot calls: "<<nb_of_calls<<endl;
+  
+  return 0;
+}
---
+++ final/molecule.cpp
@@ -0,0 +1,288 @@
+#include "molecule.h"
+
+
+//Constructors and Destructor
+
+molecule::molecule(unsigned int n_atoms, float l_box, float mute_prob) : N_atoms(n_atoms), L_box(l_box), mutation_prob(mute_prob) {
+  positions = new double*[n_atoms];
+  
+  for (int i = 0; i < n_atoms; i++)
+    positions[i] = new double[3];
+  
+  gRandom = new TRandom3(0);
+  for (int i = 0; i < N_atoms; i++){
+    for (int j = 0; j < 3; j++)
+    positions[i][j] = gRandom -> Uniform(0., l_box);
+  }
+
+  delete gRandom;
+
+  this->Fit();
+}
+
+//Neste aqui é preciso passar nmr de atomos como argumento? Não se pode só usar um getter?
+molecule::molecule(molecule* mom, molecule* dad, double gene_prop, unsigned int n_atoms): N_atoms(n_atoms) {
+  double **pos_mom = mom->Get_Pos();
+  double **pos_dad = dad->Get_Pos();
+  
+  for (int i = 0; i < N_atoms; i++){
+    for (int j = 0; j < 3; j++){
+      positions[i][j] = gene_prop * pos_mom[i][j] + (1 - gene_prop)* pos_dad[i][j];
+    }
+  }
+}
+
+molecule::~molecule() {
+  for(int i = 0; i < 3; ++i){ 
+    delete[] positions[i];
+  }
+  
+  delete[] positions;
+}
+
+//Private method that calculates fitness
+void molecule::Fit(){
+  double f = 0.;
+
+  ++nb_of_calls;
+
+  for(int i = 0; i < N_atoms-1; ++i){
+    for(int j = i+1; j < N_atoms; ++j){
+      //Calculate radius
+      double r = 0.;
+      for(int k = 0; k < 3; ++k)
+        r += (positions[i][k] - positions[j][k])*(positions[i][k] - positions[j][k]);
+      r = sqrt(r);
+
+      //Calculate Potential and sum to f_value
+      f += 4*( pow(1./r, 12) - pow(1./r, 6) );
+    }
+  }
+  
+  fitness = f;
+}
+
+
+//Setters
+
+void molecule::Set_Pos(double** new_pos){
+  for(int i=0; i<N_atoms; ++i){
+    for(int j=0; j<3; ++j)
+      positions[i][j] = new_pos[i][j];
+  }
+}
+
+void molecule::Set_Fit(double fit){
+  fitness = fit;
+}
+
+//Mutation and Reproduction
+
+void molecule::Mutate(unsigned int iter, float m0, float alpha, int flag){
+  gRandom = new TRandom3(0);
+  double check_if_mute = gRandom->Uniform(0,1);
+  
+  if(check_if_mute < mutation_prob){
+    nb_of_calls_mute ++;
+    int atom_to_mutate = (int)gRandom->Uniform(0, N_atoms);
+    for(int i=0; i<3; ++i){
+      double x0 = gRandom->Uniform(-1,1)*m0*L_box;
+      double mutation = x0/(1+alpha*iter);
+      positions[atom_to_mutate][i] += mutation;
+      if(positions[atom_to_mutate][i] < 0. || positions[atom_to_mutate][i] > L_box)
+        positions[atom_to_mutate][i] -= 2*mutation;
+    }
+    this->Fit();
+  }
+  //se nao tiver feito mutacao calcula na mesma o potencial para os que sofreram reproducao sexuada
+  else if(flag==1)
+    this->Fit();
+
+  delete gRandom;
+}
+
+
+int molecule::generate_children3(vector<molecule*> pop){
+
+  gRandom = new TRandom3(0);
+
+  double check_if_sex = gRandom->Uniform(0,1);
+
+  if(check_if_sex < sex_prob ){
+
+    //choose random parents from the surviving population
+    int mom_index = (int)gRandom->Uniform(0, parents_nb);
+    int dad_index = (int)gRandom->Uniform(0, parents_nb);
+
+    //and make sure they are different
+    while(dad_index==mom_index)
+      dad_index = (int)gRandom->Uniform(0, parents_nb);
+
+    double mating_type = gRandom->Uniform(0,1);
+
+    //call reproduction method
+    //maybe add some new ones later...
+    if(mating_type<0.9){
+      nb_of_calls_mat_plano ++;
+      this->Mating_Plano3(pop[mom_index],pop[dad_index]);
+    }
+
+    else{
+      nb_of_calls_mat ++;
+      this->Mating(pop[mom_index],pop[dad_index],gRandom->Uniform(0,1));
+    }
+    
+    delete gRandom;
+    return 1;
+  }
+  
+  delete gRandom;   
+  return 0;
+  
+}
+
+
+void molecule::Mating(molecule* mom, molecule* dad, double gene_prop){
+  double **pos_mom = mom->Get_Pos();
+  double **pos_dad = dad->Get_Pos();
+  
+  for (int i = 0; i < N_atoms; i++){
+    for (int j = 0; j < 3; j++){
+      positions[i][j] = gene_prop * pos_mom[i][j] + (1 - gene_prop)* pos_dad[i][j];
+    }
+  }
+}
+
+
+void molecule::Mating_Plano3(molecule* mom, molecule* dad){
+
+  double **pos_mom = mom -> Get_Pos();
+  double **pos_dad = dad -> Get_Pos();
+  double pos_CM = 0;
+
+  int flag = 0;
+  int nr_atoms = 0;
+
+  gRandom = new TRandom3(0);
+  int dir = (int)gRandom -> Uniform(0,3);
+
+  
+  //posicao centro de massa 
+  for(int i = 0; i < N_atoms; i++)
+    pos_CM += (pos_mom[i][dir] + pos_dad[i][dir])/2;
+  pos_CM = pos_CM/N_atoms; 
+
+  /*
+
+  cout << "Mom Atom 1 " <<  " : " <<pos_mom[0][0] << ", " << pos_mom[0][1] << ", " << pos_mom[0][2] << endl;
+  cout << "Mom Atom 2 " << " : " <<pos_mom[1][0] << ", " << pos_mom[1][1] << ", " << pos_mom[1][2] << endl;
+  cout << "Mom Atom 3 " << " : " <<pos_mom[2][0] << ", " << pos_mom[2][1] << ", " << pos_mom[2][2] << endl;
+
+  cout << "dad Atom 1 " <<  " : " <<pos_dad[0][0] << ", " << pos_dad[0][1] << ", " << pos_dad[0][2] << endl;
+  cout << "dad Atom 2 " << " : " <<pos_dad[1][0] << ", " << pos_dad[1][1] << ", " << pos_dad[1][2] << endl;
+  cout << "dad Atom 3 " <<  " : " <<pos_dad[2][0] << ", " << pos_dad[2][1] << ", " << pos_dad[2][2] << endl;
+
+  cout<<"z_cm "<<pos_CM[2]<<endl;*/
+
+
+  //choose every mom atom above CM
+  for (int i = 0; i < N_atoms; i++){
+
+
+    if(pos_mom[i][dir] > pos_CM){
+      for (int k = 0; k < nr_atoms; k++){
+        flag = 0;
+        for (int j = 0; j < 3; j++){
+          if (positions[k][j]==pos_mom[i][j])
+            flag++;
+        }
+        if(flag == 3)
+          break; 
+        }
+        if (flag != 3){
+          for(int j = 0; j < 3; j++)
+            positions[nr_atoms][j] = pos_mom[i][j];
+          nr_atoms ++;
+          //cout<<"mom  above"<<endl;
+        }
+    } 
+  }
+
+  //choose every dad atom bellow CM
+  for (int i = 0; i < N_atoms; i++){
+    if(nr_atoms== N_atoms)
+      break;
+    else if(pos_dad[i][dir] < pos_CM){
+      for(int j = 0; j < 3; j++)
+        positions[nr_atoms][j] = pos_dad[i][j];
+      nr_atoms ++;
+      //cout<<"dad bellow"<<endl;
+    }
+  }
+
+  if (nr_atoms < N_atoms){
+    //choose every mom atom below CM
+    for (int i = 0; i < N_atoms; i++){
+
+      if(pos_mom[i][dir] < pos_CM){
+        for (int k = 0; k < nr_atoms; k++){
+          flag = 0;
+          for (int j = 0; j < 3; j++){
+            if (positions[k][j]==pos_mom[i][j])
+              flag++;
+          }
+          if(flag == 3)
+            break; 
+        }
+        if (flag != 3){
+          for(int j = 0; j < 3; j++)
+            positions[nr_atoms][j] = pos_mom[i][j];
+          nr_atoms ++;
+          //cout<<"mom below"<<endl;
+        }
+      }
+      if(nr_atoms== N_atoms)
+        break;
+    }
+  }
+
+  if (nr_atoms < N_atoms){
+    //choose every dad atom above CM
+    for (int i = 0; i < N_atoms; i++){
+
+      if(pos_dad[i][dir] > pos_CM){
+        for (int k = 0; k < nr_atoms; k++){
+          flag = 0;
+          for (int j = 0; j < 3; j++){
+            if (positions[k][j]==pos_dad[i][j])
+              flag++;
+          }
+          if(flag == 3)
+            break; 
+        }
+        if (flag != 3){
+          for(int j = 0; j < 3; j++)
+            positions[nr_atoms][j] = pos_dad[i][j];
+          nr_atoms ++;
+          //cout<<"dad above"<<endl;
+        }
+      }
+      if(nr_atoms== N_atoms)
+        break;
+    }
+  }
+
+  //f_value = Potencial();
+
+  delete gRandom;
+  /*
+
+  cout<<nr_atoms<<endl;
+  cout<<"aaa"<<endl;
+
+  cout << "daughter Atom 1 " << " : " <<posicoes[0][0] << ", " << posicoes[0][1] << ", " << posicoes[0][2] << endl;
+  cout << "daughter Atom 2 " << " : " <<posicoes[1][0] << ", " << posicoes[1][1] << ", " << posicoes[1][2] << endl;
+  cout << "daughter Atom 3 " << " : " <<posicoes[2][0] << ", " << posicoes[2][1] << ", " << posicoes[2][2] << endl;
+  cout << "-------------"<<endl;*/
+
+}
